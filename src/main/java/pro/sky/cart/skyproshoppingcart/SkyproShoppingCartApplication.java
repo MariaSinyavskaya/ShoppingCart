9e2231912e3a7cee85c0200e2e7d7ba32b03ca6b
Мария <maria.sinyavskaya8@gmail.com>
2023-11-14 19:06:49 +0300

    Initial commit

---
+++ src/main/java/pro/sky/cart/skyproshoppingcart/SkyproShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.cart.skyproshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproShoppingCartApplication.class, args);
+	}
+
+}
